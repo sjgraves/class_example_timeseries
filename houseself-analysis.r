e82902ec39ff9a705925a98f831a6f567ed25e0b
Sarah Graves <sjgraves142@gmail.com>
2015-10-27 12:20:34 -0400

    Add function to determine class size from weight

--- houseself-analysis.r
+++ houseself-analysis.r
@@ -2,3 +2,11 @@
 
 # import house elf data
 dat <- read.csv("data/houseelf_earlength_dna_data.csv")
+
+get_size_class <- function(weight){
+  if (weight > 50){
+    size_class <- "large"
+  } else {
+    size_class <- "small"
+  } return(size_class)
+}
